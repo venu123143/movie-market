00b147b947b89208b4c81c6d6b72a4a8ccb5c17b
venu123143 <venugopalreddy9493@gmail.com>
2025-06-01 08:07:57 +0530

    shadcn components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './css/App.css'
+import { Button } from "@/components/ui/button"
 
-const App = () => {
+function App() {
   return (
-    <div className='bg-red-500'>App</div>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button className='cursor-pointer'>Click me</Button>
+    </div>
   )
 }
 
